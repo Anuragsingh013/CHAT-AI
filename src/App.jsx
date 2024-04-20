6657f0e15a218b63869bd65993f8b60c23176ad7
anuragsingh013 <thakuranuragsingh099@gmail.com>
2024-04-20 22:36:27 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
   }
 
   return (
-    <div className=''>
+    <div className='app-container'>
       <h1 className='title'>CHAT-AI</h1>
       <div className="app">
         <div className="answer">
